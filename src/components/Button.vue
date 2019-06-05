b600829a7e4c43acb9777f826a03dc1eecfc179e
ixap2i <ayamashita124@gmail.com>
2019-06-05 15:59:00 +0000

    add resume

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,20 +1,24 @@
 <template>
-      <v-flex xs4>
-        <v-btn color="cyan" href="https://github.com/ixap2i" class="github" target="_blank">
-          <i class="fab fa-github-alt"></i>
-          Github
-        </v-btn>
-        <v-btn color="red" href="https://www.instagram.com/umiharuno/" class="insta" target="_blank">
-          <i class="fas fa-hashtag"></i>
-          Instagram
-        </v-btn>
-        <v-btn color="green" href="https://letterneginr.hateblo.jp/" class="blog" target="_blank">
-          <i class="fas fa-rss-square"></i>
-          Blog
-        </v-btn>
-        <Modal/>
-        <Modal2/>
-      </v-flex>
+  <v-flex xs4>
+    <v-btn color="cyan" href="https://github.com/ixap2i" class="github" target="_blank">
+      <i class="fab fa-github-alt"></i>
+      Github
+    </v-btn>
+    <v-btn color="red" href="https://www.instagram.com/umiharuno/" class="insta" target="_blank">
+      <i class="fas fa-hashtag"></i>
+      Instagram
+    </v-btn>
+    <v-btn color="green" href="https://letterneginr.hateblo.jp/" class="blog" target="_blank">
+      <i class="fas fa-rss-square"></i>
+      Blog
+    </v-btn>
+    <v-btn color="gray" href="https://gist.github.com/ixap2i/7b14bfd413dc4ab3e2b680de5e898c0e" class="resume" target="_blank">
+      <i class="far fa-clipboard"></i>
+      Resume
+    </v-btn>
+    <Modal/>
+    <Modal2/>
+  </v-flex>
 </template>
 
 <script>
