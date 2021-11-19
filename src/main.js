10a31198e6a2daea249cd9be939e3706b4769e33
ixap2i <ayamashita124@gmail.com>
2021-11-19 23:35:58 +0900

    add font-awesome setting

--- src/main.js
+++ src/main.js
@@ -14,11 +14,20 @@ import '@ionic/vue/css/text-transformation.css'
 import '@ionic/vue/css/flex-utils.css'
 import '@ionic/vue/css/display.css'
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
 import { IonicVue } from '@ionic/vue'
-import { createApp } from 'vue'
+import Vue, { createApp } from 'vue'
 
 import App from './App.vue'
 import router from './router'
+
+library.add(faUserSecret)
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 const app = createApp(App)
   .use(IonicVue)
