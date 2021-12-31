412b55d1febb22b33002c6e04d5978e5574ef5f7
ixap2i <ayamashita124@gmail.com>
2021-12-31 21:28:07 +0900

    refactor data  to  map

--- src/components/organisms/InnerSkillContainer.vue
+++ src/components/organisms/InnerSkillContainer.vue
@@ -5,12 +5,12 @@
       </IonCol>
       <IonCol>
         <Title :title="'skill'" :icon="'fa fa-address-card'" />
-        <template v-for="item in items" :key="item">
+        <template v-for="(item, index) in reactiveM" :key="index" :value="item[index]">
           <SkillFilm
-            :skillName='item.skillName'
-            :skillDesc='item.desc'
-            :term='item.term'
-            :imageName='item.imageName' />
+            :skillName='item.get("skillName")'
+            :skillDesc='item.get("desc")'
+            :term='item.get("term")'
+            :imageName='item.get("imageName")' />
         </template>
       </IonCol>
       <IonCol>
@@ -23,7 +23,7 @@
 import Title from '../atoms/Title.vue'
 import SkillFilm from '../atoms/SkillFilm.vue'
 import { IonGrid, IonCol, IonRow } from '@ionic/vue'
-
+import { reactive } from 'vue'
 export default {
   components: {
     SkillFilm,
@@ -32,30 +32,33 @@ export default {
     IonCol,
     IonRow
   },
-  data () {
-    return {
-      dialog: false,
-      items: [
-        {
-          skillName: 'Ruby / Ruby on Rails',
-          desc: '',
-          term: '3年程度',
-          imageName: 'red.jpg'
-        },
-        {
-          skillName: 'Vue.js(2系)',
-          desc: 'このHPは3系です',
-          term: '1年半',
-          imageName: 'green.jpg'
-        },
-        {
-          skillName: 'Java, Kotlin',
-          desc: '',
-          subtitle: '1年程度',
-          imageName: 'blue.jpg'
-        }
-      ]
-    }
+  setup () {
+    const map1 = new Map()
+    const map2 = new Map()
+    const map3 = new Map()
+
+    map1.set('skillName', 'Ruby / Ruby on Rails')
+    map1.set('desc', '')
+    map1.set('term', '3年程度')
+    map1.set('imageName', 'red.jpg')
+
+    map2.set('skillName', 'Vue.js(2系)')
+    map2.set('desc', 'このHPは3系です')
+    map2.set('term', '1年半')
+    map2.set('imageName', 'green.jpg')
+
+    map3.set('skillName', 'Java, Kotlin')
+    map3.set('desc', '')
+    map3.set('term', '1年程度')
+    map3.set('imageName', 'blue.jpg')
+
+    const data = Array(3)
+    data.fill(map1, 0)
+    data.fill(map2, 1)
+    data.fill(map3, 2)
+
+    const reactiveM = reactive(data)
+    return { reactiveM }
   }
 }
 </script>
